3159eac6424f99f44272c741efd48eea5db09ccb
messiascardoso@gmail.com <messiascardoso@gmail.com>
2017-07-02 20:39:50 -0300

    Formulario Busca - ajuste de designer listfilme.html

--- public/js/controllers/filmes-controller.js
+++ public/js/controllers/filmes-controller.js
@@ -1,23 +1,45 @@
 angular.module('netflix').controller('FilmesCtrl',
-    function ($scope, $stateParams,Filme) {
+    function ($scope, $stateParams, Filme) {
 
-        $scope.query = 'Quentin';
+        $scope.lista = 'Quentin';
+        $scope.query = {};
         $scope.filtro = '';
+        $scope.filmes = [];
 
         //  Lista de Ofertas
-        Filme.List($scope.query)
+        Filme.List($scope.lista)
             .success(function (data) {
                 $scope.filmes = data;
                 console.log(data);
             })
             .error(function (error) {
                 console.log(error)
-             
             });
 
+        // função para enviar o formulário depois que a validação estiver ok           
+        $scope.submitForm = function (isValid) {
+            // verifica se o formulário é válido
+            if (isValid) {
+                Filme.Busca($scope.query.categoria, $scope.query.nome)
+                    .success(function (data) {
+                        if (data.length > 1) {
+                            $scope.filmes = data;
+                        } else {
+                            $scope.filmes = new Array(data);
+                        }
+                        console.log($scope.filmes);
+                    })
+                    .error(function (error) {
+                        console.log(error)
+                    });
+            }
+
+        };
 
 
-    
+      
+
+
 
 
 
@@ -31,9 +53,9 @@ angular.module('netflix').controller('FilmesCtrl',
     })
 
 
-  
 
-  
+
+
 
 
 
--- public/js/services/listfilme-service.js
+++ public/js/services/listfilme-service.js
@@ -1,11 +1,15 @@
 angular.module('netflix').service('Filme', function ($http, config) {
-    
-  
+
+
     this.List = function (director) {
-        return $http.get(config.baseUrl+"?director="+director);
+        return $http.get(config.baseUrl + "?director=" + director);
     };
 
-    
+    this.Busca = function (categoria, query ) {
+        return $http.get(config.baseUrl + "?" + categoria + "=" + query);
+    };
+
+
 
 
 
